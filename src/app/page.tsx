ba5e68ff22a3635394deec697012cd3873f24c2f
Devashri <devashribhosale1002@gmail.com>
2024-09-04 01:08:05 +0530

    Updated HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,43 @@
-import { Button } from "@/app/components/ui/button";
+
+import { auth } from "@clerk/nextjs/server";
 import { UserButton } from "@clerk/nextjs";
+import { Button } from "@/app/components/ui/button";
+import Link from "next/link";
+import {LogIn} from 'lucide-react';
+
 export default async function Home() {
+  const {userId} = await auth()
+  const isAuth = !!userId
+  
   return (
     <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
          <div className="flex flex-col items-center text-center">
-           <div className="felx items-center">
-            <h1 className="text-4xl font-bold text-gray-900">RAG.io</h1>
-            <UserButton afterSignOutUrl="/"></UserButton>        
-           </div>
-         </div>
-      </div>
+            <div className="felx items-center">
+              <h1 className="text-4xl font-bold text-gray-900">RAG.io</h1>
+              <UserButton afterSignOutUrl="/" />
+            </div>
+            <div className="flex mt-2">
+              {isAuth && <Button>Go To Chats</Button>}
+
+              <p className="max-w-xl mt-2 text-lg text-slate-600">
+                Join millions of students, researchers and professionals to instantly answer questions and understand research
+              </p>
+              <div className="w-full mt-4">
+                {isAuth ? (
+                  <h1>fileupload</h1>
+                ):(
+                  <Link href='/sign-in'>
+                  <Button>Login to get Started!
+                    <LogIn className="w-4 h-4 ml-2"/>
+                  </Button>
+                  </Link>
+                )}
+
+              </div>
+            </div>
+          </div>
+       </div>
     </div>
   )
-}
+}  

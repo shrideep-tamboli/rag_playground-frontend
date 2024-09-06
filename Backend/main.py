13cab746d9c01d8d8c607d7091e388afbb003f2a
Shrideep_Tamboli <27.tshrideep@gmail.com>
2024-09-06 19:00:18 +0530

    out of environ for api_keys

--- Backend/main.py
+++ Backend/main.py
@@ -95,14 +95,14 @@ async def process_query(request: ProcessRequest):
 
 import os
 ## Environment varibale initialization
-os.environ["NEO4J_URI"] = os.getenv("NEO4J_URI")
-os.environ["NEO4J_USERNAME"] = os.getenv("NEO4J_USERNAME")
-os.environ["NEO4J_PASSWORD"] = os.getenv("NEO4J_PASSWORD")
-os.environ["NEO4J_DATABASE"] = os.getenv("NEO4J_DATABASE")
-os.environ["groq_api_key"] = os.getenv("groq_api_key")
-os.environ["OPENAI_API_KEY"] = os.getenv('OPENAI_API_KEY2')
-os.environ["LANGCHAIN_TRACING_V2"] = "true"
-os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+NEO4J_URI = os.getenv("NEO4J_URI")
+NEO4J_USERNAME = os.getenv("NEO4J_USERNAME")
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
+NEO4J_DATABASE = os.getenv("NEO4J_DATABASE")
+groq_api_key = os.getenv("groq_api_key")
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY2')
+LANGCHAIN_TRACING_V2 = "true"
+LANGCHAIN_API_KEY = os.getenv("LANGCHAIN_API_KEY")
 
 # RAG Method Functions
 def vector_retrieval(rag_method: str, query: str, fine_tuning: Optional[FineTuning] = None):
